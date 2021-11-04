5ee5ee57cd99ee7aeca91bc19e71fd2d214950fc
JVGC <joaogarcia@usp.br>
2021-11-04 19:23:35 -0300

    [ADICIONADO] Classes HttpRequest e HttpResponse

---
+++ infra/controllers/contracts/http.py
@@ -0,0 +1,12 @@
+class HttpRequest:
+
+    def __init__(self, body=None, query=None, params=None) -> None:
+        self.body = body
+        self.query = query
+        self.params = params
+
+class HttpResponse:
+
+    def __init__(self, body, status_code) -> None:
+        self.status_code = status_code
+        self.body = body
