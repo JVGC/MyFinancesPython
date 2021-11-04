2ae12bb3a914368667856b06293840525458e3b2
JVGC <joaogarcia@usp.br>
2021-11-04 19:25:05 -0300

    [ALTERADO] Classe do Flask utilizando o FlaskAdapter.create como funcao estatica

--- infra/http/flask.py
+++ infra/http/flask.py
@@ -19,13 +19,12 @@ class FlaskApi:
     def _configure_routes(self):
 
         debt_controller =  DebtController(DebtRepositoryMongo())
-        flask_adapter= FlaskAdapter()
 
-        self.APP.add_url_rule('/debt/add', view_func=flask_adapter.create(debt_controller.add_new_debt), methods=['POST'])
-        self.APP.add_url_rule('/debt/<debt_id>', view_func=flask_adapter.create(debt_controller.get_debt_by_id), methods=['GET'])
-        self.APP.add_url_rule('/debt/pay', view_func=flask_adapter.create(debt_controller.pay_debt_part), methods=['POST'])
-        self.APP.add_url_rule('/debt/description/<debt_id>', view_func=flask_adapter.create(debt_controller.update_debt_description), methods=['PUT'])
-        self.APP.add_url_rule('/debt/delete/<debt_id>', view_func=flask_adapter.create(debt_controller.delete_debt_by_id), methods=['DELETE'])
+        self.APP.add_url_rule('/debt/add', view_func=FlaskAdapter.create(debt_controller.add_new_debt), methods=['POST'])
+        self.APP.add_url_rule('/debt/<debt_id>', view_func=FlaskAdapter.create(debt_controller.get_debt_by_id), methods=['GET'])
+        self.APP.add_url_rule('/debt/pay', view_func=FlaskAdapter.create(debt_controller.pay_debt_part), methods=['POST'])
+        self.APP.add_url_rule('/debt/description/<debt_id>', view_func=FlaskAdapter.create(debt_controller.update_debt_description), methods=['PUT'])
+        self.APP.add_url_rule('/debt/delete/<debt_id>', view_func=FlaskAdapter.create(debt_controller.delete_debt_by_id), methods=['DELETE'])
 
     def run(self):
         self.APP.run(host='0.0.0.0', port=5000, threaded=True, use_reloader=True)
