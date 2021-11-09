81feb098bd13cc56c3c81c81c9febabe3e086425
JVGC <joaovictorgarciacoelho@gmail.com>
2021-11-08 23:17:38 -0300

    [ALTERADO] Consertando erro no teste da rota de get_debt_by_id

--- domain/entities/errors/InvalidType.py
+++ domain/entities/errors/InvalidType.py
@@ -2,9 +2,11 @@ class InvalidType:
     def __init__(self, entity:str, expected_type, received_type) -> None:
 
         self.message = 'Tipo Inválido.'
+        self.expected_type = expected_type
+        self.received_type = received_type
         self.entity = entity
 
-        self.reason = f"Expected type: {expected_type} : Receveid_type: {received_type}"
+        self.reason = f"Expected type: {self.expected_type} : Receveid_type: {self.received_type}"
 
 
     def __str__(self) -> str:
--- test/controllers/test_get_debt_by_id_route.py
+++ test/controllers/test_get_debt_by_id_route.py
@@ -1,4 +1,8 @@
 import unittest
+from uuid import uuid4
+
+from pymongo import response
+from domain.entities import Date, Debt
 from infra.controllers.contracts.http import HttpRequest
 
 from infra.controllers.DebtController import DebtController
@@ -12,17 +16,37 @@ class TestGetDebtByIdRoute(unittest.TestCase):
         debt_repository_mongo = DebtRepositoryMongo()
         debt_controller = DebtController(debt_repository_mongo)
 
-        request = HttpRequest(params={'id': '123'})
-        http_response = debt_controller.add_new_debt(request)
+        _id =  str(uuid4())
 
+        debt_or_err = Debt.create(_id,
+                        'testando', 
+                        10.5,
+                        5,
+                        Date.create(year=2020, month=10).ok(),
+                        8)
+        new_debt = debt_or_err.ok()
+
+        _ = debt_repository_mongo.add(new_debt.id,
+                                        new_debt.description, 
+                                        new_debt.part_value,
+                                        new_debt.total_parts,
+                                        new_debt.start_date.to_dict(),
+                                        new_debt.total_value,
+                                        new_debt.paid_parts,
+                                        new_debt.remaining_parts,
+                                        new_debt.remaining_value)
+
+
+        request = HttpRequest(params={'debt_id': _id})
+        http_response = debt_controller.get_debt_by_id(request)
         assert http_response.status_code == 200
-        assert http_response.body['debt']['description'] == request.body['description']
+        assert http_response.body['debt']['description'] == new_debt.description
 
     def test_debt_not_found(self):
         debt_repository_mongo = DebtRepositoryMongo()
         debt_controller = DebtController(debt_repository_mongo)
 
-        request = HttpRequest(params={'id': '123'})
+        request = HttpRequest(params={'debt_id': '123'})
         http_response = debt_controller.get_debt_by_id(request)
 
         assert http_response.status_code == 404
